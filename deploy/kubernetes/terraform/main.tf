772314b971b56a0657b8bce0c72308b75be518a5
jkosla <01178539@pw.edu.pl>
2025-04-06 10:48:43 +0200

    change region

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -117,7 +117,7 @@ terraform {
     bucket       = "tf-state-bucket-jkosla"
     key          = "deploy/terraform/terraform.tfstate"
     use_lockfile = true
-    region       = "us-east-1"
+    region       = "eu-north-1"
   }
 }
 
--- deploy/kubernetes/terraform/variables.tf
+++ deploy/kubernetes/terraform/variables.tf
@@ -4,5 +4,5 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
